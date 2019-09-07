ce474c48877e9d8abd8ba46feb5ddf85f2677c88
Platholl <cyprien.plateauholleville@gmail.com>
2019-09-07 23:21:49 +0200

    Adding log system

--- include/Mirage/Core/Macro.hpp
+++ include/Mirage/Core/Macro.hpp
@@ -5,6 +5,10 @@
 #ifndef MIRAGE_MACRO_HPP
 #define MIRAGE_MACRO_HPP
 
+#include <iostream>
+
 #define MRG_UNUSED_PARAMETER(expr) do { (void)(expr); } while (0)
+#define MRG_LOG(MESSAGE, ...) std::cout << "[MRG_LOG] " << __FILE__ << ":" << __LINE__ << " " \
+                                << MESSAGE << __VA_ARGS__ << std::endl
 
 #endif //MIRAGE_MACRO_HPP
